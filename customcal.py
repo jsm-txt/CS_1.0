b49b721b21f393f93f1880e69a037cee5554f10b
Jose Salazar Mendez <thatsenpai@Joses-MBP.eau.wi.charter.com>
2020-09-03 15:53:24 -0500

    Burger Calculator
    Basic set up for burger calculator

---
+++ customcal.py
@@ -0,0 +1,21 @@
+
+contents = input("What do you want on your burger? ")
+
+print("This is what you want on your burger " + contents)
+
+numb_burgers = input("How many burgers do you want? ")
+
+numb_burgers = int(numb_burgers) 
+
+def calculate(numb_burgers):
+    cost = 5
+    tax = 1.05
+    total = 0
+    total_tax = 0
+    total = numb_burgers * cost
+    total_tax = total * tax
+    return(total_tax)
+
+total = calculate(numb_burgers)
+
+print(f'Your total is: " {total}')
