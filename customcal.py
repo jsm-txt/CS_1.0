6b82b5435ad35fbd5cfc0c6b7bd85475a0fd940a
Jose Salazar Mendez <thatsenpai@Joses-MBP.eau.wi.charter.com>
2020-09-03 16:18:30 -0500

    Added comments
    Added comments to explain my work

--- customcal.py
+++ customcal.py
@@ -1,11 +1,14 @@
+#Beginning of code, asks user for burger information
 contents = input("What do you want on your burger? ")
 
+#Prints user information to comfirm
 print("This is what you want on your burger " + contents)
 
+#Asks user for number of burgers and converts it into an int
 numb_burgers = input("How many burgers do you want? ")
-
 numb_burgers = int(numb_burgers) 
 
+#Caluclates burger cost
 def calculate(numb_burgers):
     cost = 5
     tax = 1.05
@@ -15,17 +18,19 @@ def calculate(numb_burgers):
     total_tax = total * tax
     return(total_tax)
 
+#Calls for burger cost and displays it
 total = calculate(numb_burgers)
-
 print(f'Your total is: " {total}')
 
+#Asks user for change and converts it integer
 cash = input("How much money do you have? ")
 cash = int(cash)
 
+#Calculates change
 def change(cash, total):
     change = cash - total
     return (change)
 
+#Calls and prints change
 customer_change = change(cash, total)
-
 print(f'"Your change is: " {customer_change}')
